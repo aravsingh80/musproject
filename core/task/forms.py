428761c26ac090415177b0442d0293f8192e918d
aravsingh80 <89603509+aravsingh80@users.noreply.github.com>
2022-08-05 19:25:22 -0400

    Add files via upload

---
+++ core/task/forms.py
@@ -0,0 +1,20 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, TimeField, DateField, SubmitField, SelectField
+from flask_sqlalchemy import SQLAlchemy
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+from flask import Flask
+from config import Configuration
+app = Flask(__name__)
+app.config.from_object(Configuration)
+db = SQLAlchemy(app)
+class Category(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20))
+
+    def __repr__(self):
+        return '<Category {}>'.format(self.name)
+
+class TaskForm(FlaskForm):
+    title = StringField('Name of Song', validators=[DataRequired()])
+    artist = StringField('Name of Artist', validators=[DataRequired()])
+    submit = SubmitField('Add task')

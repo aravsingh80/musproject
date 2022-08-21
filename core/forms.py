537167f065b5f3613b1a3b55531bd8d069cf328a
aravsingh80 <89603509+aravsingh80@users.noreply.github.com>
2022-08-21 12:03:29 -0400

    Update forms.py

--- core/forms.py
+++ core/forms.py
@@ -1,10 +1,11 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TimeField, DateField, SubmitField, SelectField
+from wtforms import StringField, TimeField, DateField, SubmitField, SelectField, FileField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 class TaskForm(FlaskForm):
     title = StringField('Name of Song', validators=[DataRequired()])
     artist = StringField('Name of Artist', validators=[DataRequired()])
+    audiofile = FileField()
     # year = SelectField('Year of Release', coerce=int , validators=[DataRequired()])
     # date = DateField('Date of Release', format='%Y-%m-%d' , validators=[DataRequired()])
     # time = TimeField('Time', format='%H:%M' , validators=[DataRequired()])
